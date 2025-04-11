f2347332779cd3654184ef5a9060f3e91307dd37
Bartek <bartek.kledzik@interia.pl>
2025-04-11 23:24:47 +0200

    Pokonane na 3

--- PyThomograf.py
+++ PyThomograf.py
@@ -7,41 +7,12 @@ from skimage.color import gray2rgb
 import time
 import copy
 import math
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
 from skimage.transform import radon, iradon
 from scipy.fft import fft, ifft, fftfreq
-
-#KWADRATY
-# image = imread("Kolo.jpg")# ✅  
-# image = imread("Kropka.jpg")# ✅
-# image = imread("Kwadraty2.jpg")#✅
-image = imread("Paski2.jpg")#❌ #Caly czarny obraz
-# image = imread("Shepp_logan.jpg")#✅
-#PROSTOKATY
-# image = imread("cos.jpg") #❌ -> widac kulke jednakze nie ma  bialego paska
-# image = imread("CT_ScoutView-large.jpg")#✅
-# image = imread("CT_ScoutView.jpg")#✅
-
-# image = imread("SADDLE_PE-large.jpg")#✅ Nieco wyblakle
-# image = imread("SADDLE_PE.jpg")#❌ -> caly czarny obraz
-
-
-
-image_shape0 = image.shape[0]
-image_shape1 = image.shape[1]
-
-if len(image.shape) == 3:
-    image = rgb2gray(image)
-print("DUPA: ",image.shape)
-height,width = image.shape[:2]
-print(f"Obraz: wysokość={height}, szerokość={width}")
-print(f"Lewy górny róg: (0, 0)")
-print(f"Prawy górny róg: (0, {width-1})")
-print(f"Lewy dolny róg: ({height-1}, 0)")
-print(f"Prawy dolny róg: ({height-1}, {width-1})")
-r = np.sqrt(height**2 + width**2) / 2
-centery = (width // 2)-1
-centerx = (height // 2)-1
-print(r)
 
 
 def Bresenham(x0,y0,x1,y1):
@@ -72,38 +43,28 @@ def Bresenham(x0,y0,x1,y1):
 #Musi dodac padding taki aby zamiast miec
 #Kolo z dedtektorami opisane na kwadracie aby bylo jakby wpisane w kwadrat wiekszy
 
-def addPadding(image):
-    #Obraz musi byc otoczony przez obwodke szeroka i wyskoa na dwa r minus jego aktualny wymiar
+# def addPadding(image):
+#     #Obraz musi byc otoczony przez obwodke szeroka i wyskoa na dwa r minus jego aktualny wymiar
     
-    # max_dim = max(image.shape[0],image.shape[1])
-    #result to obraz o wymiarach 2*r x 2*r
-    result = np.zeros((2*int(r),2*int(r)),dtype=image.dtype)
-    print("result shape: ",result.shape)
-    #Teraz w sam srodek result musze wrzucic image
-    x,y = (image.shape[0])//2,(image.shape[1])//2
-    print("x,y: ",x,y)
-    print("image shape: ",image.shape)
-    z = x+image.shape[0]
-    z2 = y+image.shape[1]
-    print("z,z2: ",z,z2)
-    result[int(r)-x:r-x+image.shape[0],r-y:y+image.shape[1]] = image
+#     # max_dim = max(image.shape[0],image.shape[1])
+#     #result to obraz o wymiarach 2*r x 2*r
+#     result = np.zeros((2*int(r),2*int(r)),dtype=image.dtype)
+#     print("result shape: ",result.shape)
+#     #Teraz w sam srodek result musze wrzucic image
+#     x,y = (image.shape[0])//2,(image.shape[1])//2
+#     print("x,y: ",x,y)
+#     print("image shape: ",image.shape)
+#     z = x+image.shape[0]
+#     z2 = y+image.shape[1]
+#     print("z,z2: ",z,z2)
+#     result[int(r)-x:r-x+image.shape[0],r-y:y+image.shape[1]] = image
     
-    return result
-# image2 = np.array([
-#     [1,2,3],
-#     [4,5,6]
-# ])
-# centered_image_w_padding = addPadding(image2)
-# print(centered_image_w_padding)
-def makeSquare():
-    return np.zeros((2*int(r),2*int(r)))
-# addPadding(image)
+#     return result
 
+# def makeSquare():
+#     return np.zeros((2*int(r),2*int(r)))
 
-
-# image = makeSquare()
-
-def rekonstruct(image_empty, sinogram, alfa, liczbaEm, r, centerx, centery):
+def rekonstruct(image_empty, sinogram, alpha, r, centerx, centery, filter, steps, n, l):
     """
     Rekonstruuje obraz na podstawie sinogramu metodą backprojection.
     
@@ -118,25 +79,31 @@ def rekonstruct(image_empty, sinogram, alfa, liczbaEm, r, centerx, centery):
     Zwraca:
     Zrekonstruowany obraz (normalizowany do 0-255, typu uint8).
     """ 
-    filtered_sinogram = []
-    for projection in sinogram:
-        filtered_projection = filtr(projection)
-        filtered_sinogram.append(filtered_projection)
-    plt.figure(figsize=(10, 5))
-    plt.imshow(filtered_sinogram, cmap='gray')
-    plt.title("Sinogram")
-    plt.xlabel("Pozycja detektora (kąty)")
-    plt.ylabel("Pozycja detektora (projekcje)")
-    plt.colorbar(label="Suma wartości pikseli")
-    plt.show()
-    filtered_sinogram = np.array(filtered_sinogram)
-    num_angles = filtered_sinogram.shape[0]   # liczba kątów (np. 181)
+    liczbaEm = n
+    if(filter):
+        filtered_sinogram = []
+        for projection in sinogram:
+            filtered_projection = filtr(projection)
+            filtered_sinogram.append(filtered_projection)
+        plt.figure(figsize=(10, 5))
+        plt.imshow(filtered_sinogram, cmap='gray')
+        plt.title("Sinogram po filtracji")
+        plt.xlabel("Pozycja detektora (kąty)")
+        plt.ylabel("Pozycja detektora (projekcje)")
+        plt.colorbar(label="Suma wartości pikseli")
+        plt.show()
+        filtered_sinogram = np.array(filtered_sinogram)
+           # liczba kątów (np. 181)
+        sinogram = filtered_sinogram
+    num_angles = sinogram.shape[0]
     
     for j in range(num_angles):
-        angle = j  # przy alfa=1, kąt = j stopni
+        angle = j * alpha # przy alfa=1, kąt = j stopni
         for i in range(liczbaEm):
             # Obliczanie pozycji sensora
-            pos = (i - liczbaEm / 2) / 2
+            # pos = (i - liczbaEm / 2) / 2
+            pos = (i*l/liczbaEm-l/2)
+
             # Używamy tych samych wzorów jak przy generowaniu sinogramu:
             start = (r * np.cos(np.radians(pos + angle)) + centerx,
                     r * np.sin(np.radians(pos + angle)) + centery)
@@ -155,8 +122,13 @@ def rekonstruct(image_empty, sinogram, alfa, liczbaEm, r, centerx, centery):
             cc = np.clip(cc, 0, image_empty.shape[1] - 1)
             
             # Backprojection: dodajemy wartość z sinogramu do wszystkich punktów linii
-            image_empty[rr, cc] += filtered_sinogram[j, i]
-    
+            image_empty[rr, cc] += sinogram[j, i]
+        if steps:
+            plt.imshow(image_empty, cmap='gray')
+            plt.title(f"Krok pośredni rekonstrukcji: kąt {j * alpha}°")
+            plt.axis("off")
+            plt.pause(0.01)
+            plt.clf()
     # Normalizacja zrekonstruowanego obrazu do zakresu 0-255
     image_empty -= np.min(image_empty)
     if np.max(image_empty) != 0:
@@ -206,67 +178,206 @@ def filtr(sinogram):
     filtered = np.convolve(sinogram, mask, mode='same')
     return filtered
 
+def createSinogram(image, centerx,centery,height,width,r, filter,steps,n,l,alpha):
 
-scan_1d_values = []
-liczbaEm=181
-deep=copy.deepcopy(image)
-print(centerx,centery)
-for j in range(0, 181):
-    emitter_scan = []
-    image_copy = gray2rgb(deep)
-    for i in range(0, liczbaEm):
-        pos=(i-liczbaEm/2)/2
-        # print("Pozycja detektora: ",pos)
-        start = (r * np.cos(np.radians(pos + j))+centerx, r * np.sin(np.radians(pos + j))+centery)
-        end = (r * (-1*np.cos(np.radians(-pos +j)))+centerx,r * (-1*np.sin(np.radians(-pos + j)))+centery)
-        rr, cc = Bresenham(int(start[0]), int(start[1]), int(end[0]), int(end[1]))
-        rr = np.clip(rr, 0, height - 1)
-        cc = np.clip(cc, 0, width - 1)
+    scan_1d_values = []
+    liczbaEm=n
+    deep=copy.deepcopy(image)
+    for j in range(0, 181,alpha):#Ustawienie detektorow
+        emitter_scan = []
+        image_copy = gray2rgb(deep)
+        for i in range(0, liczbaEm):#Polozenie jednego detektora dla danego ustawienia detektorow
+            pos = (i*l/liczbaEm-l/2)
 
-        Values = image[rr,cc]
-        Intensity = np.sum(Values)
+            print("Pozycja detektora: ",pos)
+            start = (r * np.cos(np.radians(pos + j))+centerx, r * np.sin(np.radians(pos + j))+centery)
+            end = (r * (-1*np.cos(np.radians(-pos +j)))+centerx,r * (-1*np.sin(np.radians(-pos + j)))+centery)
+            rr, cc = Bresenham(int(start[0]), int(start[1]), int(end[0]), int(end[1]))
+            rr = np.clip(rr, 0, height - 1)
+            cc = np.clip(cc, 0, width - 1)
 
-        emitter_scan.append(Intensity)
+            Values = image[rr,cc]
+            Intensity = np.sum(Values)
 
-
-
-        image_copy[centerx,centery]=[int(255),0,0]
-        for test in range(-5,5):
-            image_copy[centerx+test,centery]=[int(255),0,0]
-            image_copy[centerx+test,centery+test]=[int(255),0,0]
-            image_copy[centerx,centery+test]=[int(255),0,0]
-        if(pos==0):
-            image_copy[rr, cc] = [0, 255, 0]
-        else:
-            image_copy[rr, cc] = [255,255,255]
+            emitter_scan.append(Intensity)
 
 
-    plt.imshow(image_copy)
-    plt.title(f"Linia dla j={j}")
-    plt.axis("off")
-    plt.pause(0.0001)
-    plt.clf()
+            if(steps):
+                image_copy[centerx,centery]=[int(255),0,0]
+                for test in range(-5,5):
+                    image_copy[centerx+test,centery]=[int(255),0,0]
+                    image_copy[centerx+test,centery+test]=[int(255),0,0]
+                    image_copy[centerx,centery+test]=[int(255),0,0]
+                if(pos==0):
+                    image_copy[rr, cc] = [0, 255, 0]
+                else:
+                    image_copy[rr, cc] = [255,255,255]
 
-
-
-    scan_1d_values.append(emitter_scan)
+        if(steps):
+            plt.imshow(image_copy)
+            plt.title(f"Linia dla j={j}")
+            plt.axis("off")
+            plt.pause(0.001)
+            plt.clf()
 
 
 
-plt.figure(figsize=(10, 5))
-plt.imshow(scan_1d_values, cmap='gray')
-plt.title("Sinogram")
-plt.xlabel("Pozycja detektora (kąty)")
-plt.ylabel("Pozycja detektora (projekcje)")
-plt.colorbar(label="Suma wartości pikseli")
-plt.show()
+        scan_1d_values.append(emitter_scan)
 
-image_empty = np.zeros((image.shape[0],image.shape[1]),dtype=float)
-copy2 = copy.deepcopy(image)
-copy2 = rekonstruct(image_empty, np.array(scan_1d_values), 1,180,r,centerx,centery)
 
-# plt.figure(figsize=(8, 8))
 
-plt.imshow(copy2,cmap="gray")
-plt.colorbar()
-plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.imshow(scan_1d_values, cmap='gray')
+    plt.title("Sinogram")
+    plt.xlabel("Pozycja detektora (kąty)")
+    plt.ylabel("Pozycja detektora (projekcje)")
+    plt.colorbar(label="Suma wartości pikseli")
+    plt.show()
+
+    image_empty = np.zeros((image.shape[0],image.shape[1]),dtype=float)
+    copy2 = copy.deepcopy(image)
+    copy2 = rekonstruct(image_empty, np.array(scan_1d_values), 1, r, centerx, centery, filter, steps,n,l)
+
+    # plt.figure(figsize=(8, 8))
+
+    plt.imshow(copy2,cmap="gray")
+    plt.colorbar()
+    plt.show()
+
+#KWADRATY
+# image = imread("Kolo.jpg")# ✅  
+# image = imread("Kropka.jpg")# ✅
+# image = imread("Kwadraty2.jpg")#✅
+# image = imread("Paski2.jpg")#❌ #Caly czarny obraz
+# image = imread("Shepp_logan.jpg")#✅ Nieco wyblakle
+#PROSTOKATY
+# image = imread("cos.jpg") #❌ -> widac kulke jednakze nie ma  bialego paska
+# image = imread("CT_ScoutView-large.jpg")#✅
+# image = imread("CT_ScoutView.jpg")#✅
+
+# image = imread("SADDLE_PE-large.jpg")#✅ Nieco wyblakle
+# image = imread("SADDLE_PE.jpg")#❌ -> caly czarny obraz
+
+def app(image):
+    height,width = image.shape[:2]
+    r = np.sqrt(height**2 + width**2) / 2
+    centery = (width // 2)-1
+    centerx = (height // 2)-1
+
+    alpha = alphaChoice.get()
+    print(alpha)
+
+    n = nChoice.get()#Liczba Emiterow
+    print(n)
+    l = lChoice.get()#Rozpietosc kątowa
+    print(l)
+
+    Usefilter = filterChoice.get()
+    print(Usefilter)
+
+    Showsteps = stepsChoice.get()
+    print(Showsteps)
+
+    createSinogram(image,centerx,centery,height,width,r,Usefilter,Showsteps,n, l, alpha)
+
+def load_image():
+    selected_filename = imageChoice.get()
+    if selected_filename and selected_filename != "Select an image":
+        try:
+            full_path = os.path.join("Images", selected_filename)
+            image = imread(full_path)
+            print(f"Image shape: {image.shape}")
+            if len(image.shape) == 3:
+                image = rgb2gray(image)
+            app(image)
+            # plt.imshow(image, cmap='gray')
+            # plt.title(selected_filename)
+            # plt.show()
+        except Exception as e:
+            print("Error loading image:", e)
+
+
+
+root = tk.Tk()
+root.title("Symulacja tomografii komputerowej")
+root.geometry("500x400")
+
+
+images = ["Kolo.jpg", "Kropka.jpg", "Kwadraty2.jpg", "Paski2.jpg",
+        "Shepp_logan.jpg", "CT_ScoutView-large.jpg", "CT_ScoutView.jpg",
+        "SADDLE_PE-large.jpg", "SADDLE_PE.jpg"]
+
+
+imageChoice = tk.StringVar()
+
+combo = ttk.Combobox(root, values = images, textvariable=imageChoice)
+combo.set("Images")
+combo.pack(padx=10,pady=10)
+
+
+
+alphaChoice = tk.IntVar(value=1)
+alphaLabel = tk.Label(root, text="Alpha")
+alphaLabel.pack(padx=10,pady=1)
+alphaSlider = tk.Scale(root, variable=alphaChoice,from_=1,to_=10,orient="horizontal")
+alphaSlider.pack(padx=10,pady=1)
+
+
+nChoice = tk.IntVar(value=1)
+nLabel = tk.Label(root, text="Liczba detektorów")
+nLabel.pack(padx=10, pady=1)
+nSlider = tk.Scale(root,variable=nChoice,from_=1,to_=180,orient="horizontal")
+nSlider.pack(padx=10,pady=5)
+
+
+lChoice = tk.IntVar(value=1)
+lLabel = tk.Label(root, text="Rozpiętość kątowa")
+lLabel.pack(padx=10, pady=1)
+lSlider = tk.Scale(root,variable=lChoice,from_=1,to_=90,orient="horizontal")
+lSlider.pack(padx=10,pady=5)
+
+filterChoice = tk.BooleanVar(value=False)
+
+filterCheckbox = ttk.Checkbutton(root, text="Filtr", variable=filterChoice)
+filterCheckbox.pack()
+
+stepsChoice = tk.BooleanVar(value=False)
+
+stepsCheckbox = ttk.Checkbutton(root, text="Kroki Pośrednie", variable=stepsChoice)
+stepsCheckbox.pack()
+
+
+btn = tk.Button(root, text = "Choose image", command = load_image)
+btn.pack(padx=10,pady=10)
+
+root.mainloop()
+
+# image = image.get()
+# image = imread(image)
+
+# image_shape0 = image.shape[0]
+# image_shape1 = image.shape[1]
+
+
+
+# if len(image.shape) == 3:
+#     image = rgb2gray(image)
+# # print("DUPA: ",image.shape)
+# height,width = image.shape[:2]
+# # print(f"Obraz: wysokość={height}, szerokość={width}")
+# # print(f"Lewy górny róg: (0, 0)")
+# # print(f"Prawy górny róg: (0, {width-1})")
+# # print(f"Lewy dolny róg: ({height-1}, 0)")
+# # print(f"Prawy dolny róg: ({height-1}, {width-1})")
+# r = np.sqrt(height**2 + width**2) / 2
+# centery = (width // 2)-1
+# centerx = (height // 2)-1
+# print(r)
+
+
+
+# frm = ttk.Frame(root, padding=10)
+# frm.grid()
+# ttk.Label(frm, text = "Hello World!").grid(column=100, row=100)
+# ttk.Button(frm, text="Test Sinogram", command=lambda: createSinogram(image)).grid(column=2,row=2)
+# ttk.Button(frm, text="Quit", command = root.destroy).grid(column=1, row=0)
